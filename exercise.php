eb64b4c5b9eb3227080725c61341efcd5afb2675
Mindaugas <mindaugas.g@rrr.lt>
2022-04-04 20:14:31 +0300

    Add exercise

---
+++ exercise.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+ * Exercise #1
+ * Parašykite paprast clase, kuria inicijavus parodytu paprasta eileutę.
+ * -- MyClass class has initialized!
+ */
+
+class MyClass
+{
+    //..
+}
+
+/*
+ * Exercise #2
+ * Parašykite paprastą PHP klasę, kurioje būtų rodomas įvadinis pranešimas,
+ * pvz., „Sveiki, aš esu {Jusu vardas}“, kur „{Jusu vardas}“ yra klasės metodo argumento reikšmė.
+ */
+
+class Greetings
+{
+    //..
+}
+
+/*
+ * Exercise #3
+ * Parašykite PHP klasę, kuri apskaičiuoja sveikojo skaičiaus faktorialą.
+ *
+ * 2 -> 2
+ * 3 -> 6
+ * 5 -> 120
+ */
+
+class Factorial
+{
+    //...
+}
+
+/*
+ * Exercise #4
+ * Parašykite PHP klasę, kuri surūšiuoja paduoa sveikųjų skaičių masyvą nuo mažiausio iki didžiausio.
+ * Panaudokite funkciją sort().
+ *
+ * $array = [3, 5, -2, 4, -1, -3, -5, 2, 1, -4];
+ */
+
+class Sorting
+{
+
+}
+
+/*
+ * Exercise #5
+ * Parašykite PHP klasę, į kuria padavus 2 datetime objektus, parodytu skirtuma, tarp dviejų datų.
+ *
+ * DateTime("1981-11-03")
+ */
+
+class DateTimeProcessing
+{
+
+}
+
+/*
+ * Exercise #6
+ * Parašykite PHP skaičiuoklės klasę, kuri priims dvi reikšmes kaip argumentus,
+ * tada jas pridėkite, atimkite, padauginkite kartu arba padalykite.
+ *
+ * Pavyzdžiui :
+ * $calculator = new Calculator(12, 6);
+ * echo $calculator->sum(); //  18
+ * echo $calculator->multiply(); // 72
+ */
+
+class Calculator
+{
+
+}
+
+/*
+ * Exercise #8
+ * Parašykite PHP klasę, įvairių geometrinių figūrų ploto apskaičiavimui.
+ * Kvdrato, Stačiakampio, Skritulio, Trikampio plotas.
+ */
